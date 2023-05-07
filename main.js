4711406a0b4eeedd7ac952d83e45d677ef286a50
Andressa Lima <lima.andressab@outlook.com>
2023-05-07 11:44:00 -0300

    responsividade e ajustes css

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-/*const hambIcons = document.querySelector('.hamb-icons');
-let menuOpen = false;
-
-hambIcons.addEventListener('click', () => {
-    if(!menuOpen) {
-        hambIcons.classList.add('open');
-        menuOpen = true;
+function clickMenu() {
+    if (items.style.display == 'block') {
+        items.style.display = 'none'
     } else {
-        hambIcons.classList.remove('open');
-        menuOpen = false;
+        items.style.display = 'block'
     }
-});
-*/
-
-function animar() {
-    const btn = document.getElementById('icon-menu')
-    btn.classList.toggle('ative')
-}
+} 
